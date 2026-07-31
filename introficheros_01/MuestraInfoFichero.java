ef45fb5ce1777bccb3c2b3bf03d2c34cb83e460e
Mercedes Urios <mercedes.urios@educa.madrid.org>
2026-07-31 22:49:06 +0200

    variable String incluida

--- introficheros_01/MuestraInfoFichero.java
+++ introficheros_01/MuestraInfoFichero.java
@@ -3,6 +3,7 @@ package introficheros_01;
 //Comentario inicial
 public class MuestraInfoFichero {
     public static void main(String[] args) {
-        System.out.println("Hola desde main");
+        String s= "Mirentxu";
+        System.out.println("Hola " + s + " desde main");
     }
 }
