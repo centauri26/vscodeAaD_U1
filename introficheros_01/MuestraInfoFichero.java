13d386308fc264091d7b9af41f08759a646e3a6b
Mercedes Urios <mercedes.urios@educa.madrid.org>
2026-07-31 22:57:33 +0200

    incluida variable intentos

--- introficheros_01/MuestraInfoFichero.java
+++ introficheros_01/MuestraInfoFichero.java
@@ -4,6 +4,7 @@ package introficheros_01;
 public class MuestraInfoFichero {
     public static void main(String[] args) {
         String s= "Mirentxu";
-        System.out.println("Hola " + s + " desde main");
+        int intentos = 1;
+        System.out.println("Hola " + s + " desde main has realizado " + intentos + " intentos");
     }
 }
