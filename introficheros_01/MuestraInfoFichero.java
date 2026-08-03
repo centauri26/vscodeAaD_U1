d01abfcf9fa05c8ff9678f05ddc04144115ba5dd
Mercedes Urios <mercedes.urios@educa.madrid.org>
2026-08-03 10:44:46 +0200

    incluido código para listas subdirecorios y ficheros

--- introficheros_01/MuestraInfoFichero.java
+++ introficheros_01/MuestraInfoFichero.java
@@ -1,10 +1,30 @@
 package introficheros_01;
 
+import java.io.File;
+
 //Comentario inicial
 public class MuestraInfoFichero {
     public static void main(String[] args) {
-        String s= "Mirentxu";
-        int intentos = 1;
-        System.out.println("Hola " + s + " desde main has realizado " + intentos + " intentos");
+        String ruta= args.length > 0 ? args[0] : ".";
+
+        File fichero = new File(ruta);
+        if (!fichero.exists()){
+            System.out.printf("El fichero %s no existe\n", ruta);
+            return;
+        }
+        if (fichero.isFile()){
+            System.out.printf("El fichero %s es un fichero\n", ruta);
+        } else if (fichero.isDirectory()){
+            System.out.printf("El fichero %s es un directorio que contien los ficheros siguientes:\n", ruta);
+            //Como es un directorio, listo su contenido
+            File[] ficheros = fichero.listFiles();
+            for (File f : ficheros){
+                System.out.printf("  %s\n", f.getName());
+                if (f.isDirectory()){
+                    System.out.printf("/");
+                }
+                System.out.printf("%s es un directorio\n", f.getName());
+                }
+            }
+        }
     }
-}
