04a67f46056799bb5db7f313cf46d02a3fc10eab
Mercedes Urios <mercedes.urios@educa.madrid.org>
2026-08-04 11:29:16 +0200

    añadido stream.close() en el métod estático compruebaDirectorioYficheros() para liberar los recursos creados con el DirectoryStream<Path>

--- introficheros_01/MuestraInfoFicheroNIO.java
+++ introficheros_01/MuestraInfoFicheroNIO.java
@@ -24,7 +24,22 @@ public static void main(String[] args) {
     }
 
     static void compruebaDirectorioYficheros(Path directorio) {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directorio)) {
+        /* NOTA 1
+         * Hay que capturar la excepción IOException que puede lanzar 
+         * el método newDirectoryStream.
+         * ¡LO ESTUDIAREMOS DESPACIO EN UN EJERCICIO POSTERIOR!
+         * en el que estudiaremos el try-with-resources
+         */
+
+        /*
+         * NOTA 2
+         * Ver en la API26 de java.nio.file"Interface DirectoryStream<T>"
+         * <This is an object to iterate over the entries in a directory. 
+         * A directory stream allows for the convenient use of the 
+         * for-each construct to iterate over a directory>
+         * Hay que cerrar el stream al terminar de usarlo, para liberar recursos.
+         */
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(directorio)){
             for (Path entrada : stream) {
                 Path nombre = entrada.getFileName();
 
@@ -36,8 +51,8 @@ public static void main(String[] args) {
                     // Solo imprimimos como fichero si NO es un directorio (usando else)
                     System.out.printf("%s%n", nombre);
                 }
-            }
-        } catch (IOException e) {
+            } stream.close(); //¡Cerrado! cumpliendo con la NOTA 2, para liberar recursos. Se puede hacer con try-with-resources, pero lo dejamos para un ejercicio posterior.
+       } catch (IOException e) {
             System.err.printf("Error al acceder al directorio %s: %s%n", directorio, e.getMessage());
         }
     }
